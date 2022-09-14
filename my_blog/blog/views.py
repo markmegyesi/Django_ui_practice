98a232a2352a8e005bfeefdc94be8698e0b2f76a
markmegyesi <aaylasecura17@gmail.com>
2022-09-14 20:50:47 +0200

    template structure implement

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {'author': 'Megyesi Márk',
+     'title': 'Blog post 1',
+     'content': 'First content',
+     'date_posted': '2022.szeptember.14'},
+     {'author': 'Megyesi Márk',
+     'title': 'Blog post 2',
+     'content': 'second content',
+     'date_posted': '2022.szeptember.12'},
+     {'author': 'Megyesi Márk',
+     'title': 'third content',
+     'date_posted': '2022.szeptember.11'},
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 def about(request):
     return render(request, 'blog/about.html')
