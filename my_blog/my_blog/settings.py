3263493b509a4179742cdba1e5e8f091af2f6ca5
markmegyesi <aaylasecura17@gmail.com>
2022-10-01 13:43:40 +0200

    profile and css implementation

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'user_registration.apps.UserRegistrationConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
