93a57b0ee6ef28c3456e55d3158dd95101e61bb2
markmegyesi <aaylasecura17@gmail.com>
2022-10-17 17:46:18 +0200

    create post and details

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 class PostsModel(models.Model):
     title = models.CharField(max_length=100)
@@ -14,3 +14,6 @@ class PostsModel(models.Model):
 
     def __str__(self) -> str:
         return f'{self.title} - {self.author}'
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={"pk":self.pk})
--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import home, about, contact
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView
 
+                    )
 urlpatterns = [
-    path('',home, name='blog-home') ,
     path('about/',about, name='blog-about') ,
     path('contact/',contact, name='blog-contact') ,
+    # path('',home, name='blog-home') ,
+    path('',PostListView.as_view(), name='blog-home') ,
+    path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail') ,
+    path('post/new/',PostCreateView.as_view(), name='post-create') ,
 ]
