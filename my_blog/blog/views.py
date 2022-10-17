e22c1397cc283496b1c6cd73e6d415783c383129
markmegyesi <aaylasecura17@gmail.com>
2022-10-17 17:44:37 +0200

    create post and details

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import PostsModel
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 
 
@@ -16,3 +17,24 @@ def about(request):
 
 def contact(request):
     return render(request, 'blog/contact.html')
+
+class PostListView(ListView):
+    model = PostsModel
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted' ]
+
+class PostDetailView(DetailView):
+    model = PostsModel
+    template_name = 'blog/post_detail.html'
+    
+class PostCreateView(CreateView):
+    model = PostsModel
+    template_name = 'blog/post_create.html'
+    fields = ['title', 'content']
+
+    def form_valid (self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    
